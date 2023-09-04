36e00d04f6787c6ae85c8074bd55dd3b5984d24f
mpintoescalier <mpintoescalier@udesa.edu.ar>
2023-09-04 17:09:23 -0300

    screen

--- proyecto-progra3/src/App.js
+++ proyecto-progra3/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from "react-router-dom";
-import Home from './screenes/Home/Home';
+import Home from './screens/Home/Home';
 import Footer from './components/Footer/Footer.js';
 import Header from './components/Header/Header';
 
