67f4fbfcbb93ac77f5e222dcdf2d9388154ba724
Giuliana Molina <gmolina@udesa.edu.ar>
2023-09-04 16:30:40 -0300

    app

--- proyecto-progra3/src/App.js
+++ proyecto-progra3/src/App.js
@@ -1,26 +1,20 @@
-<<<<<<< HEAD
-import './App.css';
-import Home from './screenes/Home';
-=======
+import React from 'react';
+import { Switch, Route } from "react-router-dom";
+import Home from './screenes/Home/Home';
 import Footer from './components/Footer/Footer.js';
 import Header from './components/Header/Header';
->>>>>>> b8f77a57cbe840927556add1c9489c41c2544961
 
 function App() {
   return (
     <React.Fragment>
-      <Route path="/" exact={true} component={Home} />
-    <div className="App">
-<<<<<<< HEAD
-    </div>
-    </React.Fragment>
-  );
-=======
       <Header/>
+      <Switch>
+      <Route path="/" exact={true} component={Home} />
+      </Switch>
       <Footer/>
-    </div>
-  )
->>>>>>> b8f77a57cbe840927556add1c9489c41c2544961
+    </React.Fragment>
+
+  );
 }
 
 export default App;
--- proyecto-progra3/src/screenes/Home/Home.js
+++ proyecto-progra3/src/screenes/Home/Home.js
@@ -1,21 +1,21 @@
 import React from "react";
-import Footer from "../../components/Footer/Footer";
+import './Home.css';
 
 function Home() {
   return (
     <React.Fragment>
      <main>
         <h3> PELICULAS POPULARES </h3>
-        <section class="categoria1">
+        <section className="categoria1">
         </section>
         <h3> TOP RANKING SERIES  </h3>
-        <section class="categoria2">
+        <section className="categoria2">
         </section>
         <h3> TOP RANKING PELICULAS </h3>
-        <section class="categoria3">
+        <section className="categoria3">
         </section>
     </main>
-      <Footer />
+    
     </React.Fragment>
   );
 }
