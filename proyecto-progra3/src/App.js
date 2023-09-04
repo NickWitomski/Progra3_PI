a5f86d8391099f676a44c989861afe6869c32186
mpintoescalier <mpintoescalier@udesa.edu.ar>
2023-09-04 15:44:07 -0300

    Home

--- proyecto-progra3/src/App.js
+++ proyecto-progra3/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Home from './screenes/Home';
 
 function App() {
   return (
+    <React.Fragment>
+      <Route path="/" exact={true} component={Home} />
     <div className="App">
-      
     </div>
+    </React.Fragment>
   );
 }
 
---
+++ proyecto-progra3/src/screenes/Home/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Footer from "../../components/Footer/Footer";
+
+function Home() {
+  return (
+    <React.Fragment>
+     <main>
+        <h3> PELICULAS POPULARES </h3>
+        <section class="categoria1">
+        </section>
+        <h3> TOP RANKING SERIES  </h3>
+        <section class="categoria2">
+        </section>
+        <h3> TOP RANKING PELICULAS </h3>
+        <section class="categoria3">
+        </section>
+    </main>
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default Home;
