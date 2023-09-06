3dc49db06279ceac3a07b276a2bb41e220ffec12
Nicole Witomski <nwitomski@udesa.edu.ar>
2023-09-06 12:26:52 -0300

    cambios

---
+++ proyecto-progra3/src/components/Pelicula/Pelicula.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Home from "../../screens/Home/Home";
+import "./Pelicula.css"
+
+/*function Card (props){
+    return(
+        <React.Fragment>
+            <article class="articulo">
+    <img className="imagen" src={portada} alt={titulo} />
+    <p class="titulocategorias"> ${titulo} </p>
+    <p class="fecha"> ${release_date }</p>
+    </article>
+        </React.Fragment>
+    )
+}
+
+export default Pelicula; */
+
+// function Pelicula (props){
+//     return(
+//         <React.Fragment>
+//             <article className="articulo">
+//               <img className="imagen" src={`https://image.tmdb.org/t/p/w500${portada}`} alt={titulo} />
+//               <p className="titulocategorias"> ${titulo} </p>
+//               <p> {descripcion}</p>
+//             </article>
+//         </React.Fragment>
+//     )
+// }
+
+// export default Pelicula;
+
--- proyecto-progra3/src/screens/Home/Home.js
+++ proyecto-progra3/src/screens/Home/Home.js
@@ -1,37 +1,43 @@
 import React, { Component } from "react";
 import "./Home.css";
-import Card from "../../components/Card/Card";
+import Pelicula from "../../components/Pelicula/Pelicula"
+
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      peliculas:[],
-      titulo:[],
-      portada:[],
-      fecha_de_estreno:[],
-      rating:[],
-      sinopsis:[],
-      duracion:[],
-      imgUrl:""
+      pelicula: []
+      // peliculas:[],
+      // titulo:[],
+      // portada:[],
+      // fecha_de_estreno:[],
+      // rating:[],
+      // sinopsis:[],
+      // duracion:[],
+      // imgUrl:""
     }
   };
 
 componentDidMount() {
-  fetch(`https://api.themoviedb.org/3/movie/popular?api_key=399cd9827f714613d04693cee425808c&language=en-US&page=1`)
+  fetch("https://api.themoviedb.org/3/movie/popular?api_key=399cd9827f714613d04693cee425808c&language=en-US&page=1")
     .then((res) => res.json())
     .then((data) =>
       this.setState({
-        pelicula:data.results,
-        
+        pelicula: data.results,
       })
     )
     .catch(error => console.log(error));
 }
 
 render () {
+  console.log("hola")
   console.log(this.state.pelicula)
   return (
+    console.log(this.state.pelicula.title),
+    // pelicula.map((pelicula,idx) => 
+    //   // <Pelicula titulo={pelicula.title + idx} portada={pelicula.poster_path} descripcion={pelicula.overview}/>
+    // )
     <React.Fragment>
      <main>
         <h3> PELICULAS POPULARES </h3>
@@ -50,6 +56,5 @@ render () {
 }
 
 }
-
 
 export default Home;
