a7de8e9dbb33d34e7841b74378d92e64d35e0ce3
Nicole Witomski <nwitomski@udesa.edu.ar>
2023-09-06 23:53:34 -0300

    footer

--- proyecto-progra3/src/components/Footer/Footer.js
+++ proyecto-progra3/src/components/Footer/Footer.js
@@ -1,33 +1,35 @@
 import React from "react";
 import "./Footer.css"
 
-function Footer(){
-    let listaLi = ["fa-brands fa-whatsapp","fa-regular fa-envelope","fa-brands fa-instagram","fa-solid fa-link","fa-solid fa-circle-info"]
-    let listaEtiquetaP = ["Todos los derechos reservados","Copyright © 2003-2022 Fort Corporation","Marcos Pinto Escalier, Giuliana Molina y  Nicole Witomski"]
-    return(
+function Footer() {
+    let listaLi = ["fa-brands fa-whatsapp", "fa-regular fa-envelope", "fa-brands fa-instagram", "fa-solid fa-link", "fa-solid fa-circle-info"]
+    let listaEtiquetaP = ["Todos los derechos reservados", "Copyright © 2003-2022 Fort Corporation", "Marcos Pinto Escalier, Giuliana Molina y  Nicole Witomski"]
+    return (
         <React.Fragment>
-            <footer> 
-            <section>
-                <article> 
-                    {
-                        listaLi.map((elemento,idx) =>
-                            <li key={elemento + idx} className="icono"><i className={elemento}></i> </li>
-                        )
-                    }
-                    <ul className="contacto">
+            <footer>
+                <section>
+                    <article>
+
                         {
-                            listaEtiquetaP.map((elementoPe,idx) =>
+                            listaEtiquetaP.map((elementoPe, idx) =>
                                 <p className="texto2" key={elementoPe + idx}>{elementoPe} </p>
                             )
-                        } 
-                    </ul>
-                </article>
-                <article className="art2_footer">
-                        <img className="logo_tmdb" src="./img/Logo_TMDB.svg" alt="Logo_themoviedatabase"/> 
-                </article>
-            </section>
+                        }
+                        <ul className="contacto">
+                            {
+                                listaLi.map((elemento, idx) =>
+                                    <li key={elemento + idx} className="icono"><i className={elemento}></i> </li>
+                                )
+                            }
+                        </ul>
+                    </article>
+                    <article className="art2_footer">
+                        <img className="logo_tmdb" src="./img/Logo_TMDB.svg" alt="Logo_themoviedatabase" />
+                    </article>
+                </section>
             </footer>
         </React.Fragment>
-    )}
+    )
+}
 
 export default Footer;
