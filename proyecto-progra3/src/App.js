b311a17f3ed093541abd1ab55bc5aa4fc2fa77c1
mpintoescalier <mpintoescalier@udesa.edu.ar>
2023-09-07 13:01:41 -0300

    Noencontrado

--- proyecto-progra3/src/App.js
+++ proyecto-progra3/src/App.js
@@ -4,16 +4,17 @@ import Home from './screens/Home/Home.js';
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header';
 import Favoritos from './screens/Favoritos/Favoritos.js';
+import NoEncontrada from './screens/NoEncontrado/noEncontrado.js';
 // import detallePelicula from './screens/DetallePelicula/detallePelicula';
 
 function App() {
   return (
     <React.Fragment>
       <Header/>
-      <Home/>
       <Switch>
       <Route path="/" exact={true} component={Home} />
-      <Route path="/favoritos" component = {Favoritos}/> 
+      <Route path="/favoritos" component={Favoritos}/> 
+      <Route component={NoEncontrada}></Route>
       {/* <Route path="/detallePelicula" component={detallePelicula}/> */}
       </Switch>
       <Footer/>
---
+++ proyecto-progra3/src/screens/NoEncontrado/noEncontrado.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NoEncontrada() {
+  return (
+    <React.Fragment>
+      <h1>Error 404 no se encontro esta pagina</h1>
+    </React.Fragment>
+  );
+};
+
+export default NoEncontrada;
