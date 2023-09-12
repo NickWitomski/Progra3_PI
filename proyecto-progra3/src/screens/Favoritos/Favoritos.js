9792236143770be76236dc2df3d2e361dc031504
Giuliana Molina <gmolina@udesa.edu.ar>
2023-09-12 00:15:15 -0300

    fav

--- proyecto-progra3/src/screens/Favoritos/Favoritos.js
+++ proyecto-progra3/src/screens/Favoritos/Favoritos.js
@@ -6,35 +6,58 @@ class Favoritos extends Component {
     constructor(props){
         super(props);
         this.state = {
-            peliFav: [],
+            favs: [],
             id: this.props.match.params.id,
         }
         console.log(this.props.match.params.id)
-        console.log(this.state.peliFav)
+        console.log(this.state.favs)
     };
 
     componentDidMount(){
-        console.log('Mount')
-        fetch(`https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=399cd9827f714613d04693cee425808c&language=en-US`)
-        .then((res) => res.json())
-        .then((data) =>
+        let favs = [];
+        let recuperoStorage = localStorage.getItem('favs')
+
+        if (recuperoStorage !== null) {
+
+            let storageToArray = JSON.parse(recuperoStorage)
+            favs = storageToArray
+        }
+        let peliculasFavs = [];
+
+        favs.map(unIdDelArray => (
+            fetch(`https://api.themoviedb.org/3/movie/${unIdDelArray}?api_key=d1566b6a7005fc1288c0cf8495a15e2e&language=en-US`)
+            .then(response => response.json())
+            .then(data => {
+                peliculasFavs.push(data);
+                this.setState({
+                    favs: peliculasFavs,
+                })
+            })
+            .catch(error => console.log(error)) 
+            ))
+        }
+        
+    borrarPelicula(idPeliABorrar){
+        let arrayFiltrado = this.state.favs.filter(unaPeli => unaPeli.id !== idPeliABorrar)
+        
         this.setState({
-            peliFav: data,
+            favs: arrayFiltrado
         })
-        )
-        .catch(error => console.log(error))
     }
+                
+            
+    
 
     render(){
         return(
             console.log('Traigo las pelis'),
-            console.log(this.state.peliFav),
+            console.log(this.state.favs),
             <React.Fragment>
-                <h3 className="titulo1"> FAVORITOS:</h3>
+                <h3 className="titulo1"> FAVORITOS</h3>
                 <section className="container">
                 <article className="articulo"> 
-                    <h2> ${this.state.peliFav.title}</h2>
-                        <img className="imagen" src={`https://image.tmdb.org/t/p/w500/${this.state.peliFav.poster_path}`} alt={`${this.state.peliFav.title}`} />
+                    <h2> {this.state.favs.title}</h2>
+                        <img className="imagen" src={`https://image.tmdb.org/t/p/w500/${this.state.favs.poster_path}`} alt={`${this.state.favs.title}`} />
                 </article>
                 </section> 
             </React.Fragment>
